24e226ec2efd33bc18d0f13bd1d01d02052340a8
Vincent Lee <vlee2@nevada.unr.edu>
2022-03-24 23:44:40 -0700

    boolean checks and makefile

--- main.cpp
+++ main.cpp
@@ -12,6 +12,15 @@ int main() {
   account.makeWithdrawal(Money(201,34));
   std::cout << account << std::endl;
 
-
+  std::cout << "==========================" << std::endl;
+  std::cout << "Relations between " << startingBalance << " and " << account.getBalance() << std::endl;
+  std::cout << "--------------------------" << std::endl;
+  std::cout << std::boolalpha;
+  std::cout << "Is " << startingBalance << " > " << account.getBalance() << ": " << (startingBalance > account.getBalance()) << std::endl;
+  std::cout << "Is " << startingBalance << " < " << account.getBalance() << ": " << (startingBalance < account.getBalance()) << std::endl;
+  std::cout << "Is " << startingBalance << " >= " << account.getBalance() << ": " << (startingBalance >= account.getBalance()) << std::endl;
+  std::cout << "Is " << startingBalance << " <= " << account.getBalance() << ": " << (startingBalance <= account.getBalance()) << std::endl;
+  std::cout << "Is " << startingBalance << " == " << account.getBalance() << ": " << (startingBalance == account.getBalance()) << std::endl;
+  std::cout << "Is " << startingBalance << " != " << account.getBalance() << ": " << (startingBalance != account.getBalance()) << std::endl;
   return 0;
 }
---
+++ makefile
@@ -0,0 +1,14 @@
+test: main.o Money.o Account.o
+	g++ -std=c++11 -o test main.o Money.o Account.o
+
+main.o: main.cpp Account.h
+	g++ -std=c++11 -c main.cpp
+
+Money.o: Money.cpp Money.h
+	g++ -std=c++11 -c Money.cpp
+
+Account.o: Account.cpp Account.h Money.h
+	g++ -std=c++11 -c Account.cpp
+
+clean:
+	rm *.o test
